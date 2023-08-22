f9c52deb60576131a4ce6241b85c67f235d691c2
Mayadah <97852885+MayadahAA@users.noreply.github.com>
2023-08-22 11:36:53 +0300

    commit

---
+++ my-project/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl w-fit'>
+          <h1 className='text-white text-2xl'>
+            hello
+          </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
